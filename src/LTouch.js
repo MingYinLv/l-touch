87a31326fa1e1590b9eb668e35f7534cf94cd8df
lvmingyin <lvmingyin@vip.qq.com>
2016-08-08 16:04:20 +0800

    'min'

--- src/LTouch.js
+++ src/LTouch.js
@@ -7,6 +7,7 @@
         root.LTouch = factory;
     }
 })(window, function (domObj, option) {
+
     var params = {
         touchLeft: function () {    //左滑
         },
@@ -35,49 +36,48 @@
         moveX: 0,
         moveY: 0
     };
-    domObj.addEventListener("touchstart", function (event) {
-
+    domObj.addEventListener('touchstart', function (event) {
         touchObj.startX = event.changedTouches[0].pageX;
         touchObj.startY = event.changedTouches[0].pageY;
-        params.touchStart.call(domObj,event, touchObj.startX, touchObj.startY);
+        params.touchStart(event, touchObj.startX, touchObj.startY);
     });
-    domObj.addEventListener("touchmove", function (event) {
+    domObj.addEventListener('touchmove', function (event) {
         touchObj.moveX = event.changedTouches[0].pageX - touchObj.startX;
         touchObj.moveY = touchObj.startY - event.changedTouches[0].pageY;
-        params.touchMove.call(domObj,event, touchObj.moveX, touchObj.moveY);
+        params.touchMove(event, touchObj.moveX, touchObj.moveY);
     });
-    domObj.addEventListener("touchend", function (event) {
+    domObj.addEventListener('touchend', function (event) {
         touchObj.endX = event.changedTouches[0].pageX;
         touchObj.endY = event.changedTouches[0].pageY;
         if (touchObj.startX - touchObj.endX > params.offset) { //left
 
-            params.touchLeft.call(domObj,event, touchObj.endX - touchObj.startX);
+            params.touchLeft(event, touchObj.endX - touchObj.startX);
         }
         if (touchObj.endX - touchObj.startX > params.offset) { //right
-            params.touchRight.call(domObj,event, touchObj.endX - touchObj.startX);
+            params.touchRight(event, touchObj.endX - touchObj.startX);
         }
         if (touchObj.startY - touchObj.endY > params.offset) { //down
-            params.touchDown.call(domObj,event, touchObj.startY - touchObj.endY);
+            params.touchDown(event, touchObj.startY - touchObj.endY);
         }
         if (touchObj.endY - touchObj.startY > params.offset) { //up
-            params.touchUp.call(domObj,event, touchObj.startY - touchObj.endY);
+            params.touchUp(event, touchObj.startY - touchObj.endY);
         }
         params.touchEnd(event, touchObj.endX - touchObj.startX, touchObj.startY - touchObj.endY);
     });
-    domObj.addEventListener("touchcancel", function () {
+    domObj.addEventListener('touchcancel', function () {
         if (touchObj.startX - touchObj.endX > params.offset) { //left
-            params.touchLeft.call(domObj,event, touchObj.endX - touchObj.startX);
+            params.touchLeft(event, touchObj.endX - touchObj.startX);
         }
         if (touchObj.endX - touchObj.startX > params.offset) { //right
-            params.touchRight.call(domObj,event, touchObj.endX - touchObj.startX);
+            params.touchRight(event, touchObj.endX - touchObj.startX);
         }
         if (touchObj.startY - touchObj.endY > params.offset) { //down
-            params.touchDown.call(domObj,event, touchObj.startY - touchObj.endY);
+            params.touchDown(event, touchObj.startY - touchObj.endY);
         }
         if (touchObj.endY - touchObj.startY > params.offset) { //up
-            params.touchUp.call(domObj,event, touchObj.startY - touchObj.endY);
+            params.touchUp(event, touchObj.startY - touchObj.endY);
         }
-        params.touchEnd.call(domObj,event, touchObj.endX - touchObj.startX, touchObj.startY - touchObj.endY);
+        params.touchEnd(event, touchObj.endX - touchObj.startX, touchObj.startY - touchObj.endY);
     });
 
 });
